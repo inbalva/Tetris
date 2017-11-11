cc36500ecef042c82d5b85e42507c3fe7c21d7fa
shaniyu <33570326+shaniyu@users.noreply.github.com>
2017-11-11 20:07:29 +0200

    Block.h
    Header file for "Block" class

---
+++ Block.h
@@ -0,0 +1,28 @@
+﻿#ifndef _BLOCK_H
+#define _BLOCK_H
+
+#include "Point.h"
+
+class Block {
+private:
+	Point body[4];
+	char c;
+	
+public:
+
+	//צריך להגריל פה צורה מתוך 4 צורות אפשריות
+
+};
+
+void Block::draw()
+{
+	
+}
+
+void Block::move(int dirX, int dirY)
+{
+	x = x + dirX;
+	y = y + dirY;
+	draw();
+}
+#endif // _BLOCK_H
